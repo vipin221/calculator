e42a39d6646222c09598338b2497fd3ef2cf4e0f
vipin221 <115896995+vipin221@users.noreply.github.com>
2023-04-26 22:44:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let input = document.getElementById('input');
+let button = document.querySelectorAll('button');
+
+let string  ="";
+
+let arr = Array.from(button);
+arr.forEach(button =>{
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            if(string%1 ==0){
+                input.value = string;
+            }
+            else{
+                input.value = string.toFixed(2);
+            }
+           
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
+
+var str = '45+2';
+
+str = eval(str);
+if((str%1))
+console.log(str%1);
